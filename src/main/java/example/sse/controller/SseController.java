648d42feaecbb5b3b7a1eafe4d84c5135b27a1e9
Sijun <sionwer4@naver.com>
2023-06-27 13:16:04 +0900

    feat: Create server sent event controller and service

---
+++ src/main/java/example/sse/controller/SseController.java
@@ -0,0 +1,37 @@
+package example.sse.controller;
+
+import example.sse.service.SseService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Slf4j
+@RestController
+public class SseController {
+
+    private final SseService sseService;
+
+    @Autowired
+    public SseController(SseService sseService) {
+        this.sseService = sseService;
+    }
+
+    @CrossOrigin
+    @GetMapping(value="/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public SseEmitter connect(){
+        Integer userId = 1;
+        SseEmitter sseEmitter = sseService.add(userId);
+        return sseEmitter;
+    }
+
+    @CrossOrigin
+    @PostMapping("/sse")
+    @ResponseStatus(HttpStatus.OK)
+    public void order(){
+        sseService.count();
+    }
+}
---
+++ src/main/java/example/sse/service/SseService.java
@@ -0,0 +1,54 @@
+package example.sse.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+@Service
+public class SseService {
+
+    private static final AtomicLong emitterCounter = new AtomicLong();
+    ConcurrentHashMap<Integer, SseEmitter> emitterList = new ConcurrentHashMap<>();
+
+
+
+    public SseEmitter add(Integer userId) {
+        SseEmitter emitter;
+        emitter = new SseEmitter();
+        emitterList.put(userId, emitter);
+        log.info("new emitter added: {}", emitter);
+        log.info("emitter list:{}", emitterList);
+        emitter.onCompletion(() -> {
+            this.emitterList.remove(emitter);
+            log.info("emitter deleted: {}", emitter);
+        });
+        emitter.onTimeout(emitter::complete);
+        long count = emitterCounter.get();
+        try{
+            emitter.send(SseEmitter.event().name("connect").data("connected"));
+            emitter.send(SseEmitter.event().name("count").data(count));
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        return emitter;
+    }
+
+    public void count() {
+        long count = emitterCounter.incrementAndGet();
+        emitterList.forEachValue(Long.MAX_VALUE, emitter -> {
+            try {
+                emitter.send(SseEmitter.event().name("count").data(count));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+}
